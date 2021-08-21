d9aeb04e4fc01a8e31255c26e1875d1bd28ea638
Milena <milena20182018@gmail.com>
2021-08-21 19:20:57 -0300

    Cabeçalho do sidebar

--- aplication/lib/sidebar/sidebar.dart
+++ aplication/lib/sidebar/sidebar.dart
@@ -16,7 +16,6 @@ class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin<S
   Stream<bool> isSidebarOpenedStream;
   StreamSink<bool> isSidebarOpenedStreamSink;
 
-  //final bool isSidebarOpened = false;
   final _animationDuration = const Duration(milliseconds: 500);
 
   @override
@@ -59,13 +58,43 @@ class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin<S
           duration: _animationDuration,
           top: 0,
           bottom: 0,
-          left: isSidebarOpenedAsync.data ? 0 : 0,
+          left: isSidebarOpenedAsync.data ? 0 : -screenWidth, // -screenWidth : Ao encolher a tela os componentes de dentro do SB nao saem do lugar
           right: isSidebarOpenedAsync.data ? 0 : screenWidth - 45,
           child: Row(
             children: [
               Expanded(
                   child: Container(
                     color: Colors.amber,
+                    child: Column(
+                      children: [
+                        SizedBox(height: 100),
+                        ListTile(
+                          title: Text(
+                            "Milena",
+                            style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w800),
+                          ),
+                          subtitle: Text(
+                            "milena20182018@gmail.com",
+                            style: TextStyle(color: Colors.amber.shade100, fontSize: 15),
+                          ),
+                          leading: CircleAvatar(
+                            /*child: Icon(
+                              Icons.perm_identity,
+                              color: Colors.white,
+                            ),*/
+                            radius: 40,
+                            backgroundImage: AssetImage("assets/perfil.jpeg"),
+                          ),
+                        ),
+                        Divider( // Onde cria a linha horizontal que separa o cabeçalho do corpo do SB
+                          height: 64,
+                          thickness: 0.5,
+                          color: Colors.white.withOpacity(0.8),
+                          indent: 32,
+                          endIndent: 32,
+                        )
+                      ],
+                    ),
                   )
               ),
               Align(
